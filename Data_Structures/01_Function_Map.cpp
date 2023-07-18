1906bc7ed96e24428545588103850b49335c7969
Rachit Garg <rachitgarg900@gmail.com>
2023-07-18 20:04:53 +0200

    Update and rename 01_Map_Function.cpp to 01_Function_Map.cpp

---
+++ Data_Structures/01_Function_Map.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <functional>
+#include <map>
+#include <string>
+
+void func_one()
+{
+    std::cout << "Function 1 executed :)" << std::endl;
+}
+
+void func_two()
+{
+    std::cout << "Function 2 executed <<>>" << std::endl;
+}
+
+void func_three()
+{
+    std::cout << "Function 3 executed :>" << std::endl;
+}
+
+int main()
+{
+    std::string input;
+    
+    // creating a map for mapping function names to their respective functions
+    std::map<std::string, std::function<void(void)>> func_map;
+
+    // filling functions with their respective keys into the map
+    func_map["one"] = func_one;
+    func_map["two"] = func_two;
+    func_map["three"] = func_three;
+
+    do 
+    {
+        std::cout << "Enter the function name to be executed (quit to exit): ";
+        std::cin >> input;
+
+        if (func_map.find(input) != func_map.end())
+        {
+            func_map[input]();
+        }
+    } while (input != "quit");
+   
+}   
