6cd33bb0d3133b2f4a4e52fd1bb95c549775da62
Rachit Garg <rachitgarg900@gmail.com>
2024-06-16 18:43:37 +0200

    Added comments in special_point.h

--- PImpl_Idiom/special_point.h
+++ PImpl_Idiom/special_point.h
@@ -6,10 +6,14 @@ class SpecialPoint
 {
 public:
     explicit SpecialPoint(float x, float y);
+    // It is mandatory to mention destructor but not implement here
+    // This is because, SpecialPointImpl's size is not known for destructing unique ptr, hence
+    // default destructor cannot be used right away. So we tell the compiler that it will be implemented somewhere for now 
+    // (in our special_point.cpp file where it is known)
     ~SpecialPoint();
     void print_point();
 
 private:
-    struct SpecialPointImpl;
-    std::unique_ptr<SpecialPointImpl> pImpl;
+    struct SpecialPointImpl; // Forward declaration of type used for PImpl
+    std::unique_ptr<SpecialPointImpl> pImpl; // Our Pointer to Implementation (PImpl)
 };
