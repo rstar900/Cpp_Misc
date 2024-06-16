6e3d6939511ef008660ddafd8fe0665ee4e87024
Rachit Garg <rachitgarg900@gmail.com>
2024-06-16 23:13:07 +0200

    Added comments to special_point.cpp and updated print_points()

--- Idioms/01.PImpl/special_point.cpp
+++ Idioms/01.PImpl/special_point.cpp
@@ -3,9 +3,20 @@
 
 struct SpecialPoint::SpecialPointImpl 
 {
+    // So instead of revealing the private members via header file
+    // they can simply be declared in this struct and hence, when
+    // provided as a precompiled library, the user only gets the header
+    // without any obvious revealation of private members
     float x;
     float y;
     float z;
+
+    // Same applies to functions, then the class object just calls the function within
+    // the pImpl object via indirection as shown below in SpecialPoint::print_point()
+    print_point()
+    {
+        std::cout << "x: " << x << ", y: " << y << ", z: " << z << std::endl;
+    }
 };
 
 SpecialPoint::SpecialPoint(float x, float y)
@@ -17,5 +28,5 @@ SpecialPoint::~SpecialPoint(){}
 
 void SpecialPoint::print_point()
 {
-    std::cout << "x: " << pImpl->x << ", y: " << pImpl->y << ", z: " << pImpl->z << std::endl;
+    pImpl->print_point();
 }
